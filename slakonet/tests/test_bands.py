35f0dba98db780777e77dfed4f8681be809aa9c1
user <writetokamal.1989@gmail.com>
2025-09-06 12:38:41 -0400

    Pytest

--- slakonet/tests/test_bands.py
+++ slakonet/tests/test_bands.py
@@ -1,9 +1,10 @@
 from slakonet.get_bands import get_gap
 from slakonet.optim import MultiElementSkfParameterOptimizer
+import os
 
+model_path=os.path.join(os.path.dirname(__file__),"slakonet_v1_sic")
 def test_si():
 
- model_path="slakonet_v1_sic"
  model = MultiElementSkfParameterOptimizer.load_model(model_path, method='state_dict')
  get_gap(jid='JVASP-1002',model=model,plot=True)
 
